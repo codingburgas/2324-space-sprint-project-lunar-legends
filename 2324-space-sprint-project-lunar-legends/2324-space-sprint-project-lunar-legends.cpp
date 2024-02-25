010525515d0f3416ce9764293633ae743d9bb7bf
IMIvanov22 <IMIvanov22@codingburgas>
2024-02-25 15:48:31 +0200

    edit file structure

--- 2324-space-sprint-project-lunar-legends/2324-space-sprint-project-lunar-legends.cpp
+++ 2324-space-sprint-project-lunar-legends/2324-space-sprint-project-lunar-legends.cpp
@@ -1,20 +1,38 @@
-// 2324-space-sprint-project-lunar-legends.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "Header Files/menu.h"
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    //int choice;
+
+    menu(choice);
+        //cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+        
+            cout << "Case1" << endl;
+            break;
+        
+        case 2:
+        
+            cout << "Case2" << endl;
+            break;
+        
+        case 3:
+        
+            cout << "Case3" << endl;
+            break;
+        
+        case 4:
+        
+            cout << "End of Program" << endl;
+            break;
+        
+        default:
+            cout << "Not a Valid Choice." << endl;
+            cout << "Choose again." << endl;
+            break;
+        }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ 2324-space-sprint-project-lunar-legends/Source/menu.cpp
@@ -0,0 +1,14 @@
+#include "../Header Files/menu.h"
+
+
+void menu(int choice) 
+{
+    
+        cout << endl;
+        cout << " 1 - Start the game - Easy" << endl;
+        cout << " 2 - Start the game - Normal" << endl;
+        cout << " 3 - Start the game - Hard" << endl;
+        cout << " 4 - Exit" << endl;
+        cout << " Enter your choice and press return: " << endl;
+        cin >> choice;
+}
