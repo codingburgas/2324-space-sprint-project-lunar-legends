aaa6949acc7a321891d1a2d40b50bf014979f491
IMIvanov22 <IMIvanov22@codingburgas.bg>
2024-02-26 11:10:43 +0200

    edit file structure

--- 2324-space-sprint-project-lunar-legends/Source/menu.cpp
+++ 2324-space-sprint-project-lunar-legends/Source/menu.cpp
@@ -1,14 +1,21 @@
 #include "../Header Files/menu.h"
 
 
-void menu(int choice) 
+void menu(int choice)
 {
-    
-        cout << endl;
-        cout << " 1 - Start the game - Easy" << endl;
-        cout << " 2 - Start the game - Normal" << endl;
-        cout << " 3 - Start the game - Hard" << endl;
-        cout << " 4 - Exit" << endl;
-        cout << " Enter your choice and press return: " << endl;
-        cin >> choice;
+
+    cout << endl;
+    cout << " 1 - Start the game - Easy" << endl;
+    cout << " 2 - Start the game - Normal" << endl;
+    cout << " 3 - Start the game - Hard" << endl;
+    cout << " 4 - Exit" << endl;
+    cout << " Enter your choice and press return: " << endl;
+    cin >> choice;
+
+    if (choice == 1)
+        void questionsEasy();
+    else if (choice == 2)
+        void questionsNormal();
+    else if (choice == 3)
+        void questionsHard();
 }
